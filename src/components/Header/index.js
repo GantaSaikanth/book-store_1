5078400170d1d4e08472a3ced22916538363675c
GantaSaikanth <gsaikanth5678@gmail.com>
2025-01-23 14:09:45 +0530

    Added static version of Home and Header and Maked Header responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,39 @@
+import { Component } from 'react'
+
+import {Link} from 'react-router-dom'
+
+import { FaBook } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
 import './index.css'
 
-const Header = () => (
-    <nav>
-        <h1>Header</h1>
-    </nav>
-)
+class Header extends Component {
+    render() {
+        return (
+            <nav className='nav-bar'>
+                <Link className="links" to="/">
+                    <h1>BookStore</h1>
+                </Link>
+                <div className='link-container'>
+                    <Link className='links' to="books">
+                        <div className='link-to-booklist'>
+                            <FaBook />
+                            <span className='span-children'>BookList</span>
+                        </div>
+                    </Link>
+                    <Link className='links' to="cart">
+                        <div className='link-to-cart'>
+                            <FaShoppingCart />
+                            <span className='span-children'>Cart</span>
+                        </div>
+                    </Link>
+                    <button className='logout-button'>Logout</button>
+                </div>
+            </nav>
+        )
+    }
+}
+    
+
 
 export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,10 +7,18 @@ import './index.css'
 class Home extends Component {
     render() {
         return (
-        <div>
+        <>
             <Header />
-            <h1>Home container</h1>
-        </div>
+            <div className='book-store-container'>
+                <div>
+                    <h1 className='heading'>Book Store</h1>
+                    <p className='paragraph'>A book is now a set of printed sheets of paper held together between two covers. The sheets of paper in a book are called pages. The pages have words written in them and maybe illustrations drawn. The first books were not printed, but written by hand in ink.The book is a more flexible format than the earlier idea of the scroll. The change from scrolls to books began in the Roman Empire and took many centuries to become complete.A writer of a book is often called an author. Someone who draws the pictures in a book is called an illustrator. Books can have more than one writer or illustrator.</p>
+                    <button className='explore-button'>
+                        Explore Books
+                    </button>
+                </div>
+            </div>
+        </>
         )
     }
 }
